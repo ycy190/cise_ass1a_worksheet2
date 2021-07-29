ecf166c34d7eb3ac35876c3627202f22341db7e5
ycy190 <ycy190@163.com>
2021-07-29 16:18:30 +1200

    q34

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-     <h1>Welcome to CISE - the home of learning and fun</h1>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      count: 0,
+    }
+  }
+
+  makeIncrementer = amount => () =>
+    this.setState(prevState => ({
+      count: prevState.count + amount,
+    }));
+
+  increment = this.makeIncrementer(1);
+
+  render() {
+    return (
+      <div>
+        <p>Count: {this.state.count}</p>
+        <button className="increment" onClick={this.increment}>Increment count</button>
+      </div>
+    )
+  }
 }
 
 export default App;
